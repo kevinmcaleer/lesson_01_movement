c81cf9e3f053c5573af1ca517d074ebbdda9a454
Kevin McAleer <kevinmcaleer@me.com>
2019-11-03 21:18:30 +0000

    Improved readability

--- lesson01.ino
+++ lesson01.ino
@@ -1,38 +1,54 @@
 // Lesson 01 Movement
+// www.smarsfan.com/play/lessons/lesson_01_movement
 
-int motor_A = 12; // set Motor A to Arduino Pin 12
-int motor_B = 13; // set Motor B to Arduino Pin 13
-int motor_A_speed = 10; // set Motor A Speed to Arduino Pin 10
-int motor_B_speed = 11; // set Motor B Speed to Arduino Pin 11
+// set Motor A to Arduino Pins
+int motor_A = 12;
+int motor_B = 13;
 
-int wait_in_milliseconds = 1000; // set the time between motor on and motor off
+// set the Motor Speed using the Arduino Pins
+int motor_A_speed = 10;
+int motor_B_speed = 11;
 
+// set the time between motor on and motor off
+int wait_in_milliseconds = 1000;
 
+// this code runs once at the start
 void setup() {
-  // this code runs once at the start
+
+  // this sets the speed of communication between the computer and Arduino,
+  // used when uploading your code
   Serial.begin(9600);
 
+  // set the Arduino pin to OUTPUT mode
   pinMode(motor_A, OUTPUT);
   pinMode(motor_B, OUTPUT);
 }
 
+// move forward
 void forward() {
 
-  // move forward
-  digitalWrite(motor_A, LOW);  // set the direction to forward
-  digitalWrite(motor_B, HIGH); // set the direction to forward
+  // set the direction to forward
+  digitalWrite(motor_A, LOW);
+  digitalWrite(motor_B, HIGH);
 
-  analogWrite(motor_A_speed, 255); // set to full speed
-  analogWrite(motor_B_speed, 255); // set to full speed
+  // set to full speed
+  analogWrite(motor_A_speed, 255);
+  analogWrite(motor_B_speed, 255);
 
-  delay(wait_in_milliseconds); // wait
+  // wait
+  delay(wait_in_milliseconds);
 
-  analogWrite(motor_A_speed, 0); // stop
-  analogWrite(motor_B_speed, 0); // stop
+  // stop
+  analogWrite(motor_A_speed, 0);
+  analogWrite(motor_B_speed, 0);
 }
 
 // the main program loop
 void loop(){
-  forward(); // move forward
-  delay(2000); // wait 2 seconds
+
+  // move forward
+  forward();
+
+  // wait 2 seconds
+  delay(2000);
 }
